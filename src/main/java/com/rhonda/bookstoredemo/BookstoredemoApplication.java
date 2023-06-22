d615726c6f4bad0a10a68e10750dd3086a7d10bc
Rhonda  Toh <rhonda.toh.e@thalesdigital.io>
2023-06-22 15:04:46 +0800

    chore: initial commit

---
+++ src/main/java/com/rhonda/bookstoredemo/BookstoredemoApplication.java
@@ -0,0 +1,13 @@
+package com.rhonda.bookstoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoredemoApplication.class, args);
+	}
+
+}
